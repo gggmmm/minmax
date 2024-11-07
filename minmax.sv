13cc01758690bb0cb1581b0ae2efcb80d13b3c21
giovanni <olimareloojie@gmail.com>
2024-11-07 18:49:23 +0100

    only unsigned supported. input xis now an array.

--- minmax.sv
+++ minmax.sv
@@ -2,14 +2,13 @@
 
 module minmax #(
     parameter integer W         = 5,            // input data width
-    parameter integer NI        = 64,            // Number of inputs
+    parameter integer NI        = 64,           // Number of inputs
     parameter integer IDXW      = $clog2(NI),   // num. bits to represent and index
     parameter integer OUT_CFG   = 1,            // 0= output both value and index, 1= only value
-    parameter integer MM_CFG    = 1,            // 0= both min max, 1= only min, 2=only max
-    parameter integer US_CFG    = 1             // 0= support both signed and unsigned, 1= only unsigned, 2= only signed
+    parameter integer MM_CFG    = 1             // 0= both min max, 1= only min, 2=only max
 )(
-    input   logic [NI*W-1 : 0] x,
-    input   logic us_sel, min_max_sel,
+    input   logic [W-1:0] x [NI],
+    input   logic min_max_sel,
 
     output  logic [W-1:0] result,
     output  logic [IDXW-1:0] index
@@ -30,10 +29,9 @@ module minmax #(
 
     localparam treeDepth = $clog2(NI);
     generate
-        logic [W-1:0]    intermediate        [treeDepth-1:0][NI/2:0];
-        logic [IDXW-1:0] intermediateIdx     [treeDepth-1:0][NI/2:0];
+        logic [W-1:0]    intermediate       [treeDepth-1:0][NI/2:0];
+        logic [IDXW-1:0] intermediateIdx    [treeDepth-1:0][NI/2:0];
         logic            int_min_max_sel;
-        logic            int_us_sel;
 
         if (MM_CFG==0)
             assign int_min_max_sel = min_max_sel;
@@ -41,13 +39,6 @@ module minmax #(
             assign int_min_max_sel = 1'b0; // only min
         else if(MM_CFG==2)
             assign int_min_max_sel = 1'b1; // only max
-
-        if(US_CFG==0)
-            assign int_us_sel = us_sel;
-        else if(US_CFG==1)
-            assign int_us_sel = 1'b0; // only unsigned
-        else if(US_CFG==2)
-            assign int_us_sel = 1'b1; // only signed
 
         for(genvar i=0; i<treeDepth; i=i+1) begin : vertical
             for(genvar j=0; j<f_NumInputFromPreiousLevel(i, NI); j=j+2) begin : horizontal
@@ -57,78 +48,78 @@ module minmax #(
                     if( (f_NumInputFromPreiousLevel(i, NI) % 2 == 0)   ||
                         (j+1) < (f_NumInputFromPreiousLevel(i, NI)-1)    ) begin : even_or_not_last
                         
-                        if(OUT_CFG==0)
+                        if(OUT_CFG==0) begin : value_and_index
                             minmax2 #(.IDXW(IDXW), .W(W), .OUT_CFG(OUT_CFG)) u_mm2(
-                                      .x            (x[W*(j+1)-1:W*j]           ),
-                                      .y            (x[W*(j+2)-1:W*(j+1)]       ),
-                                      .xi           (j                          ),
-                                      .yi           (j+1                        ),
-                                      .us_sel       (int_us_sel                 ),
+                                      .x            (x[j]                       ),
+                                      .y            (x[j+1]                     ),
+                                      .xi           (j                          ), // throws warning because j is 32 bit, while port has IDXW bit
+                                      .yi           (j+1                        ), // throws warning because j is 32 bit, while port has IDXW bit
                                       .min_max_sel  (int_min_max_sel            ),
                                       .result       (intermediate[i][j/2]       ),
                                       .index        (intermediateIdx[i][j/2]    ) // same index as result
                                     );
-                        else if(OUT_CFG==1)
+                        end else if(OUT_CFG==1) begin : only_value
                             minmax2 #(.IDXW(IDXW), .W(W), .OUT_CFG(OUT_CFG)) u_mm2(
-                                  .x            (x[W*(j+1)-1:W*j]           ),
-                                  .y            (x[W*(j+2)-1:W*(j+1)]       ),
-                                  .us_sel       (int_us_sel                 ),
+                                  .x            (x[j]                       ),
+                                  .y            (x[j+1]                     ),
                                   .min_max_sel  (int_min_max_sel            ),
                                   .result       (intermediate[i][j/2]       )
                                 );
+                        end
                     end else begin : odd_and_last
                         // just skip, this result is alone and needs to be just moved to the next level
-                        if(OUT_CFG==0)
+                        if(OUT_CFG==0) begin : value_and_index
                             PassThrough #(.IDXW(IDXW), .W(W), .OUT_CFG(OUT_CFG)) u_pt(
-                                .value_in   (x[W*(j+1)-1:W*j]           ),
+                                .value_in   (x[j]                       ),
                                 .index_in   (j                          ),
                                 .result     (intermediate[i][j/2]       ),
                                 .index      (intermediateIdx[i][j/2]    )
                             );
-                        else if(OUT_CFG==1)
+                        end else if(OUT_CFG==1) begin : only_value
                             PassThrough #(.IDXW(IDXW), .W(W), .OUT_CFG(OUT_CFG)) u_pt(
-                                .value_in   (x[W*(j+1)-1:W*j]           ),
+                                .value_in   (x[j]                       ),
                                 .result     (intermediate[i][j/2]       )
                             );
+                        end
                     end
                 end else begin : other_level // any other level, use intermediate variable
 
                     if( (f_NumInputFromPreiousLevel(i, NI) % 2 == 0) ||
                         (j+1) < (f_NumInputFromPreiousLevel(i, NI)-1)  ) begin : even_or_not_last
 
-                        if(OUT_CFG==0)
+                        if(OUT_CFG==0) begin : value_and_index
                             minmax2 #(.IDXW(IDXW), .W(W), .OUT_CFG(OUT_CFG)) u_mm2(
                                       .x            (intermediate[i-1][j]       ),
                                       .y            (intermediate[i-1][j+1]     ),
                                       .xi           (intermediateIdx[i-1][j]    ), // same index as x
                                       .yi           (intermediateIdx[i-1][j+1]  ), // same index as y
-                                      .us_sel       (int_us_sel                 ),
                                       .min_max_sel  (int_min_max_sel            ),
                                       .result       (intermediate[i][j/2]       ),
                                       .index        (intermediateIdx[i][j/2]    )  // same index as result
                                     );
-                        else if(OUT_CFG==1)
+                        end else if(OUT_CFG==1) begin : only_value
                             minmax2 #(.IDXW(IDXW), .W(W), .OUT_CFG(OUT_CFG)) u_mm2(
                                       .x            (intermediate[i-1][j]       ),
                                       .y            (intermediate[i-1][j+1]     ),
-                                      .us_sel       (int_us_sel                 ),
                                       .min_max_sel  (int_min_max_sel            ),
                                       .result       (intermediate[i][j/2]       )
                                     );
+                        end
                     end else begin : odd_and_last
                         // just skip, this result is alone and needs to be just moved to the next level
-                        if(OUT_CFG==0)
+                        if(OUT_CFG==0) begin : value_and_index
                             PassThrough #(.IDXW(IDXW), .W(W), .OUT_CFG(OUT_CFG)) u_pt(
                                 .value_in   (intermediate[i-1][j]       ),
                                 .index_in   (intermediateIdx[i-1][j]    ),
                                 .result     (intermediate[i][j/2]       ),
                                 .index      (intermediateIdx[i][j/2]    )
                             );
-                        else if(OUT_CFG==1)
+                        end else if(OUT_CFG==1) begin : only_value
                             PassThrough #(.IDXW(IDXW), .W(W), .OUT_CFG(OUT_CFG)) u_pt(
                                 .value_in   (intermediate[i-1][j]   ),
                                 .result     (intermediate[i][j/2]   )
                             );
+                        end
                     end
                 end
             end
@@ -166,31 +157,22 @@ endmodule // passThrough
 // Notice how the index is chosen, always compared first with x,
 // hence if x is equal to y, x index is passed forward.
 module minmax2 #(
-    parameter IDXW      = 2,
-    parameter W         = 6,
+    parameter IDXW      = 3,
+    parameter W         = 8,
     parameter OUT_CFG   = 0
 )(
     input   logic [W-1 : 0]     x, y,
     input   logic [IDXW-1 : 0]  xi, yi,
-    input   logic               us_sel, min_max_sel,
+    input   logic               min_max_sel,
 
     output  logic [W-1 : 0]     result,
     output  logic [IDXW-1 : 0]  index
 );
-    // perhaps no need to propagate MM_CFG and US_CFG here?
-    // I would expect the synthesis tool to propagate the constants and eliminate unreachable cases.
-    // Need to be confirmed with some synthesis results.
-    // it might speed up simulation tho
-    assign result = us_sel == 0 ? min_max_sel == 0  ? $unsigned(x) < $unsigned(y)   ? x // unsigned, min
-                                                                                    : y
-                                                    : $unsigned(x) > $unsigned(y)   ? x // unsigned, max
-                                                                                    : y
-                                : min_max_sel == 0  ? $signed(x) < $signed(y)       ? x // signed, min
-                                                                                    : y
-                                                    : $signed(x) > $signed(y)       ? x // signed, max
-                                                                                    : y;
+    assign result = min_max_sel==1'b1 ? x > y ? x : y
+                                      : x < y ? x : y;
+
     generate
         if(OUT_CFG==0)
-            assign index = result == x ? xi : yi;
+                assign index = result == x ? xi : yi;
     endgenerate
 endmodule
--- tb_minmax.sv
+++ tb_minmax.sv
@@ -1,8 +1,7 @@
 // Testbench for the minmax module
 // Uncomment the define DEBUG to have a some informative output in the console.
 // Adjust parameter NUM_TESTS to increase/decrease number of tests. Each test is carried out both for
-// signed and unsigned casese as well as min and max search. Therefore, a total of
-// NUM_TESTS * 2 * 2 will be carried out.
+// min and max search. Therefore, a total of NUM_TESTS * 2 will be carried out.
 
 `timescale 1ns / 1ns
 
@@ -10,16 +9,15 @@
 
 module tb_minmax();
 
-    localparam integer W            = 5;
+    localparam integer W            = 8;
     localparam integer NI           = 9;
     localparam integer OUT_CFG      = 0; // 0= output both value and index, 1= only value
-    localparam integer MM_CFG       = 1; // 0= support both min/max, 1= only min, 2= only max
-    localparam integer US_CFG       = 1; // 0= support both signed and unsigned, 1= only unsigned, 2= only signed
+    localparam integer MM_CFG       = 0; // 0= support both min/max,        1= only min,    2= only max
     localparam integer IDXW         = $clog2(NI);
-    localparam integer NUM_TESTS    = 250;
+    localparam integer NUM_TESTS    = 500;
 
-    logic [NI*W-1 : 0] x;
-    logic us_sel, min_max_sel;
+    logic [W-1:0] x [NI];
+    logic min_max_sel;
     logic [W-1:0] result;
     logic [IDXW-1:0] index;
 
@@ -27,176 +25,73 @@ module tb_minmax();
 
     time del = 1ns;
     initial begin
-        static int errormins = 0, errormaxs = 0;
         static int errorminu = 0, errormaxu = 0;
 
-        logic [W-1:0]       ar;
-        logic [NI*W-1 : 0]  arand;
-        logic [W-1:0]       min, max, idxmin, idxmax;
+        logic [W-1:0] min, max, idxmin, idxmax;
 
-        int num_of_loops;
+        automatic int mm_num_loops = MM_CFG==0 ? 2 : 1;
 
-        if(US_CFG==0)
-            num_of_loops = 2;
-        else if(US_CFG==1 || US_CFG==2)
-            num_of_loops = 1;
-
-        for (int i = 0; i < num_of_loops; i++) begin
-            if(US_CFG==0) begin
-                if(i==0)
-                    us_sel = 1'b0;
-                else if(i==1)
-                    us_sel = 1'b1;
-            end else if(US_CFG==1 || US_CFG==2) begin
-                if(US_CFG==1)
-                    us_sel = 1'b0;
-                else if(US_CFG==2)
-                    us_sel = 1'b1;
-            end
+        for( int h=0; h < mm_num_loops; h++) begin
+            if(MM_CFG==0)
+                min_max_sel = h;
+            else
+                min_max_sel = MM_CFG == 1 ? 0 : 1;
 
             for( int j = 0; j < NUM_TESTS; j++ ) begin
                 `ifdef DEBUG $display("== TEST ",j," =="); `endif
 
-                arand = '0;
-                if( us_sel == 1'b1 ) begin
-                    min = 2**(W-1)-1;
-                    max = -2**(W-1);
-                end else begin
-                    min = 2**W-1;
-                    max = -2**W;
-                end
+                min = 2**W-1;
+                max = 0;
                 idxmin = 0;
                 idxmax = 0;
                 for( int k=0; k < NI; k++ ) begin
-                    ar = $urandom_range(2**W-1, 0);
+                    x[k] = $urandom_range(2**W-1, 0);
 
                     `ifdef DEBUG
-                        if( us_sel == 1'b1 )
-                            $display("%d %d", $signed(ar), NI-k-1);
-                        else
-                            $display("%d %d", $unsigned(ar), NI-k-1);
+                            $display("%d %d", x[k], k);
                     `endif
 
-                    arand = {arand, ar};
-
-                    if( us_sel == 1'b1 ) begin
-                        if($signed(ar) >= $signed(max)) begin
-                            // update the index if they are equal and the new one is smaller than the previous. 
-                            // in case there are two equal values, hardware "picks" always the smallest index, 
-                            // as a side effect of its implementation
-                            if($signed(ar) == $signed(max) && NI-k-1 < idxmax)
-                                idxmax = NI-k-1;
-                            else
-                                idxmax = NI-k-1;
-                            max = $signed(ar);
-                        end
-                        if($signed(ar) <= $signed(min)) begin
-                            // update the index if they are equal and the new one is smaller than the previous. 
-                            // in case there are two equal values, hardware "picks" always the smallest index, 
-                            // as a side effect of its implementation
-                            if($signed(ar) == $signed(min) && NI-k-1 < idxmin)
-                                idxmin = NI-k-1;
-                            else
-                                idxmin = NI-k-1;
-                            min = $signed(ar);
-                        end
-                    end else begin
-                        if($unsigned(ar) >= $unsigned(max)) begin
-                            // update the index if they are equal and the new one is smaller than the previous. 
-                            // in case there are two equal values, hardware "picks" always the smallest index, 
-                            // as a side effect of its implementation
-                            if($unsigned(ar) == $unsigned(max) && NI-k-1 < idxmax)
-                                idxmax = NI-k-1;
-                            else
-                                idxmax = NI-k-1;
-                            max = $unsigned(ar);
-                        end
-                        if($unsigned(ar) <= $unsigned(min)) begin
-                            // update the index if they are equal and the new one is smaller than the previous. 
-                            // in case there are two equal values, hardware "picks" always the smallest index, 
-                            // as a side effect of its implementation
-                            if($unsigned(ar) == $unsigned(min) && NI-k-1 < idxmin)
-                                idxmin = NI-k-1;
-                            else
-                                idxmin = NI-k-1;
-                            min = $unsigned(ar);
-                        end
+                    if(x[k] > max) begin
+                        max = x[k];
+                        idxmax = k;
+                    end
+                    if(x[k] < min) begin
+                        min = x[k];
+                        idxmin = k;
                     end
                 end
 
-                x = arand;
+                #del;
 
-                if(MM_CFG==0 || MM_CFG==1) begin // min
-                    min_max_sel = 0;
-                    #del;
-
-                    // checking min
-                    if(us_sel==1'b1) begin // signed
-                        assert($signed(result) == $signed(min) && ((OUT_CFG == 0) && index == idxmin) || (OUT_CFG == 1))
-                            `ifndef DEBUG
-                                ;
-                            `else
-                                    $display("PASS"); 
-                                else begin 
-                                    $display("FAIL"); $display("result: ", $signed(result), " index: ", index, " min: ", $signed(min), " idxmin: ", idxmin); errormins++;
-                                end
-                            `endif
-                    end else begin // unsigned
-                        assert($unsigned(result) == $unsigned(min) && ((OUT_CFG == 0) && index == idxmin) || (OUT_CFG == 1))
-                            `ifndef DEBUG
-                                ;
-                            `else
-                                    $display("PASS");
-                                else begin 
-                                    $display("FAIL"); $display("result: ", $unsigned(result), " index: ", index, " min: ", $unsigned(min), " idxmin: ", idxmin); errorminu++;
-                                end
-                            `endif
-                    end
-                end
-
-                if(MM_CFG==0 || MM_CFG==2) begin // max
-                    min_max_sel = 1;
-                    #del;
-
-                    // checking max
-                    if(us_sel==1'b1) begin // signed
-                        assert($signed(result) == $signed(max) && ((OUT_CFG == 0) && index == idxmax) || (OUT_CFG == 1))
-                            `ifndef DEBUG
-                                ;
-                            `else
-                                    $display("PASS");
-                                else begin 
-                                    $display("FAIL"); $display("result: ", $signed(result), " index: ", index, " max: ", $signed(max), " idxmax: ", idxmax); errormaxs++;
-                                end
-                            `endif
-                    end else begin // unsigned
-                        assert($unsigned(result) == $unsigned(max) && ((OUT_CFG == 0) && index == idxmax) || (OUT_CFG == 1))
-                            `ifndef DEBUG
-                                ;
-                            `else
-                                    $display("PASS");
-                                else begin 
-                                    $display("FAIL"); $display("result: ", $unsigned(result), " index: ", index, " max: ", $unsigned(max), " idxmax: ", idxmax); errormaxu++;
-                                end
-                            `endif
-                    end
+                if(min_max_sel==1'b0) begin // min
+                    assert(result == min && ((OUT_CFG == 0) && index == idxmin) || (OUT_CFG == 1))
+                        `ifndef DEBUG
+                            else errorminu++;
+                        `else
+                                $display("PASS");
+                            else begin
+                                $display("FAIL"); $display("result: ", result, " index: ", index, " min: ", min, " idxmin: ", idxmin); errorminu++;
+                            end
+                        `endif
+                end else begin // max
+                    assert(result == max && ((OUT_CFG == 0) && index == idxmax) || (OUT_CFG == 1))
+                        `ifndef DEBUG
+                            else errormaxu++;
+                        `else
+                                $display("PASS");
+                            else begin
+                                $display("FAIL"); $display("result: ", result, " index: ", index, " max: ", max, " idxmax: ", idxmax); errormaxu++;
+                            end
+                        `endif
                 end
             end
         end
 
-        if(MM_CFG==0 || MM_CFG==1) begin // min
-            if(US_CFG==0 || US_CFG==2) // signed
-                $display("MIN - SIGNED . ERRORS ", errormins, "/", NUM_TESTS);
-            if(US_CFG==0 || US_CFG==1) // unsigned
-                $display("MIN - UNSIGNED ERRORS ", errorminu, "/", NUM_TESTS);
-        end
+        if(MM_CFG==0 || MM_CFG==1)
+                $display("MIN - ERRORS ", errorminu, "/", NUM_TESTS);
 
-        if(MM_CFG==0 || MM_CFG==2) begin
-            if(US_CFG==0 || US_CFG==2) // signed
-                $display("MAX - SIGNED . ERRORS ", errormaxs, "/", NUM_TESTS);
-            if(US_CFG==0 || US_CFG==1)
-                $display("MAX - UNSIGNED ERRORS ", errormaxu, "/", NUM_TESTS);
-        end
+        if(MM_CFG==0 || MM_CFG==2)
+                $display("MAX - ERRORS ", errormaxu, "/", NUM_TESTS);
     end
 
 endmodule
